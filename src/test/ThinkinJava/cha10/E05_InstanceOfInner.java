4060f9b769551cbc319d08353a55192bf7afd663
wusq121 <6352194152qq.com>
2018-05-05 19:17:02 +0800

    update

---
+++ src/test/ThinkinJava/cha10/E05_InstanceOfInner.java
@@ -0,0 +1,16 @@
+package test.ThinkinJava.cha10;
+
+class Outer3{
+	class Inner{
+		{System.out.println("Inner created");}
+	}
+}
+
+public class E05_InstanceOfInner {
+
+	public static void main(String[] args) {
+		Outer3 o = new Outer3();
+		Outer3.Inner oi = o.new Inner();
+	}
+
+}
